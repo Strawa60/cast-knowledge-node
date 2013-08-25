469a5c2d94625ee24e3d157c733de4d39c891e12
mstrawczynski@gmail.com <mstrawczynski@gmail.com@e67a7849-ab8b-de9d-bad7-cdd5b02e5171>
2013-08-25 16:42:42 +0000

    dodany Wzorzec MVVM, usunięte zbędne klasy i zedytowane wymagane, dodane pliki access doman xml'a

---
+++ TestApp/KsiegarniaCDA.GUI/KsiegarniaCDA.GUI.SL/Model/DataService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using KsiegarniaCDA.GUI.SL.KsiegarniaCDAServiceReference;
+
+namespace KsiegarniaCDA.GUI.SL.Model
+{
+    public class DataService : IDataService
+    {
+        private readonly ServiceClient _sericeClient;
+
+
+        public DataService()
+        {
+            _sericeClient = new ServiceClient();
+
+        }
+
+    }
+}
