8a421b676beb73fe0fc175d0070cba3cfa51ddf9
mstrawczynski <mstrawczynski@gmail.com>
2014-03-04 17:32:11 +0000

    utworzony szkielet rozwiązań z podstawową konfiguracją referencji i NugetPacks. Dodana abstrakcja fabryki kontrolera DI (Dependency Injection NINJECT)

---
+++ CastKnowledge/CastKnowledge.WebUI/Infrastructure/NinjectControllerFactory.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ninject;
+using System.Web.Routing;
+
+namespace CastKnowledge.WebUI.Infrastructure
+{
+    public class NinjectControllerFactory : DefaultControllerFactory
+    {
+        private IKernel ninjectKernel;
+
+        public NinjectControllerFactory()
+        {
+            ninjectKernel = new StandardKernel();
+            AddBindings();
+        }
+
+        protected override IController GetControllerInstance(RequestContext requestContext, Type controllerType)
+        {
+            return controllerType == null ? null : (IController)ninjectKernel.Get(controllerType);
+        }
+
+        private void AddBindings()
+        {
+            //dodatkowe powiązania
+            /*
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new List<Product> 
+            {
+                new Product { Name = "Piłka nożna", Price = 25},
+                new Product { Name = "Deska Surfingowa", Price = 179},
+                new Product { Name = "buty do biegania", Price= 95}
+            }.AsQueryable());
+
+            ninjectKernel.Bind<IProductRepository>().ToConstant(mock.Object);
+            */
+
+            //ninjectKernel.Bind<IProductRepository>().To<EFProductRepository>();
+
+
+        }
+
+    }
+}
