ba86b8da87701a2a38460ccb1ac67f229ebaac56
mstrawczynski <mstrawczynski@gmail.com>
2014-03-20 18:50:31 +0000

    poprawiony model dostawcow, poprawiona imitacja bazy, dodany model odlewni
    TODO: skonczyć cały model

--- CastKnowledge/CastKnowledge.Domain/Entities/DatabaseQueryResult.cs
+++ CastKnowledge/CastKnowledge.Domain/Entities/DatabaseQueryResult.cs
@@ -1,11 +1,13 @@
 ﻿using CastKnowledge.Domain.Abstract;
 using CastKnowledge.Domain.Entities.ContractorModel;
+using CastKnowledge.Domain.Entities.PublicationModel;
 
 namespace CastKnowledge.Domain.Entities
 {
     public class DatabaseQueryResult
     {
-        public Contractor QueryResult;
+        public Contractor QueryResult_Contractor;
+        public Publication QueryResult_Publication;
 
     }
 }
---
+++ CastKnowledge/CastKnowledge.Domain/Entities/PublicationModel/Publication.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CastKnowledge.Domain.Entities.PublicationModel
+{
+    public class Publication
+    {
+        public string title { get; set; }
+        public string content { get; set; }
+    }
+}
