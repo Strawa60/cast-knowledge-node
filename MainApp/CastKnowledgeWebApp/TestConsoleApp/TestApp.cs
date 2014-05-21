91136dbe0418d157720aa5cbb2d34e1e9bf2db37
mstrawczynski@gmail.com <mstrawczynski@gmail.com@e67a7849-ab8b-de9d-bad7-cdd5b02e5171>
2014-05-21 22:05:15 +0000

    added import from excel to site, added resources

--- MainApp/CastKnowledgeWebApp/TestConsoleApp/TestApp.cs
+++ MainApp/CastKnowledgeWebApp/TestConsoleApp/TestApp.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SyrinxMvc.ExcelParserEngine;
+using CastKnowledgeWebApp.Domain;
 
 namespace TestConsoleApp
 {
@@ -11,7 +13,9 @@ namespace TestConsoleApp
         static void Main(string[] args)
         {
 
+            //List<Dostawca> myList = new List<Dostawca>();
 
+            //myList = ExcelParser.ParseContractorData();
             
 
         }
